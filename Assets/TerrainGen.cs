f896b2bd537e0d3b030ec0f17ff90c21dbabc3fa
Insculpo <tkaufeld@ucsc.edu>
2018-12-10 01:00:56 -0800

    where did meta go

--- Assets/TerrainGen.cs
+++ Assets/TerrainGen.cs
@@ -19,16 +19,16 @@ public class TerrainGen : MonoBehaviour {
 	public float treeDensity;
 	public float treeColorAdjustment = 0.4f;
 	float[,] heightField;
-
-
+	//List<Vector2> treeList;
 	// Use this for initialization
 	void Start () {
 		//biomeMap.Resize (AAlps.detailWidth, AAlps.detailHeight);
-
+	//	treeList = new Vector2<float>();
 		AAlps.size = new Vector3(width, depth, width);
 		heightField = new float[width,width];
 		GenerateTerrain (biomeMap,AAlps);
 		GenerateBiomeTexture (biomeMap,AAlps);
+		PlaceTreesAcrossTerrain (100, biomeMap);
 	}
 
 	// Update is called once per frame
@@ -80,23 +80,21 @@ public class TerrainGen : MonoBehaviour {
 			for (int x = 0; x < aM.alphamapWidth; x++) {
 				float normX = x * 1.0f / (aM.alphamapWidth - 1);
 				float normY = y * 1.0f / (aM.alphamapHeight - 1);
+				float abAngle = aM.GetSteepness (normX, normY);
+				float tShift = abAngle / 90f;
 
 				float[] splatW = new float[aM.alphamapLayers];
 
 				Vector3 norm = aM.GetInterpolatedNormal (x, y);
 
-				if(bM.GetPixel(x,y).g < 0.5) {
+				if (bM.GetPixel (x, y).g > 0.5) {
+					float zv = -0.5f;
+					splatW [2] = zv + (tShift * 2);
+				}
 						//Normalizes 
-						splatW [0] = 0.0f;
-						splatW [1] = 1.0f;
+						splatW [0] = 1 - (bM.GetPixel(x,y).g * 0.8f);
+						splatW [1] = bM.GetPixel(x,y).g ;
 
-				}
-				else
-				{
-						//Normalizes 
-					splatW [0] = 1.0f;
-					splatW [1] = 0.0f;
-				}
 				float z = splatW.Length;
 				for (int i = 0; i < splatW.Length; i++) {
 					splatW [i] /= z;
@@ -109,5 +107,92 @@ public class TerrainGen : MonoBehaviour {
 			}
 		}
 		aM.SetAlphamaps (0, 0, newData);
+	}
+
+
+
+	public void PlaceTreesAcrossTerrain(int numTrees, Texture2D bM){
+		float treeX = 0;
+		float treeZ = 0;
+		float trueTree = numTrees;
+		for (int i = 0; i < trueTree; i++) {
+			if (bM.GetPixel(treeX, treeZ).g < 0.5) {
+				treeX = Random.Range (0, 1f);
+				treeZ = Random.Range (0, 1f);
+				PlaceTree (treeX, treeZ);
+				trueTree--;
+			} 
+		}
+	}
+
+	void PlaceTree(float tX, float tY) {
+		//	TreeInstance newTree;
+		int numPrototypes = aM.terrainData.treePrototypes.Length;
+		float tAngle = aM.terrainData.GetSteepness (tX, tY);
+		float tFrac = tAngle / 90.0f;
+		//int t = Random.Range (numPrototypes-2, numPrototypes);
+		if (numPrototypes == 0) {
+			Debug.Log ("No trees!");
+			return;
+		}
+		//Propability based placement
+		int p = Random.Range (0, 99);
+
+		int b = Random.Range (0.5, 1.5);
+		//Occassional MEGA trees!
+		if (p > 90) {
+			b = Random.Range (4.5, 5.5);
+		}
+		float r = Random.Range (0f, 6.238f);
+		if (tFrac < 0.32f && b < 4) {
+			Vector3 pos = new Vector3 (tX, 0, tY);
+			TreeInstance newTree = new TreeInstance ();
+			newTree.position = pos;
+			newTree.color = GetTreeColor ();
+			newTree.lightmapColor = Color.white;
+			newTree.prototypeIndex = 0;
+			newTree.heightScale = b;
+			newTree.widthScale =  b/2;
+			newTree.rotation = r;
+			terrain.AddTreeInstance (newTree);
+			treeField.Add (newTree);
+		}
+
+	}
+
+	private Color GetTreeColor() {
+		Color color = Color.white * Random.Range (1f, 1f - treeColorAdjustment);
+		color.a = 1f;
+		return color;
+	}
+
+
+	public void newOctave(float o){
+		octaves = Mathf.RoundToInt(o);		
+	}
+	public void newHeight(float h){
+		depth = Mathf.RoundToInt(h);	
+	}
+	public void newPersistance(float p){
+		persistance = p;	
+	}
+	public void newscale(float s){
+		scale = s;	
+	}
+
+/*	void killTree(float x, float y)
+	{
+		for(i =
+			if(x == treeList[i].)
+			{
+			}
+	}*/
+
+	float GetSum(float[] g) {
+		float mean = 0f;
+		for (int i = 0; i < g.Length; i++) {
+			g [i] += mean;
+		}
+		return mean / g.Length;
 	}
 }
